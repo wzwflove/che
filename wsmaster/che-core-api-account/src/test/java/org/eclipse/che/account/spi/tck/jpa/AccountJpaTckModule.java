608a0dadb017b3fdc88c90a18b8c777f6f2d7866
Yevhenii Voevodin <evoevodin@codenvy.com>
2016-11-03 17:03:41 +0200

    Use schema for account

--- wsmaster/che-core-api-account/src/test/java/org/eclipse/che/account/spi/tck/jpa/AccountJpaTckModule.java
+++ wsmaster/che-core-api-account/src/test/java/org/eclipse/che/account/spi/tck/jpa/AccountJpaTckModule.java
@@ -16,21 +16,27 @@ import com.google.inject.persist.jpa.JpaPersistModule;
 import org.eclipse.che.account.spi.AccountDao;
 import org.eclipse.che.account.spi.AccountImpl;
 import org.eclipse.che.account.spi.jpa.JpaAccountDao;
+import org.eclipse.che.commons.test.db.H2JpaCleaner;
+import org.eclipse.che.commons.test.db.H2SchemaLoader;
 import org.eclipse.che.commons.test.tck.TckModule;
 import org.eclipse.che.commons.test.tck.TckResourcesCleaner;
-import org.eclipse.che.commons.test.tck.JpaCleaner;
 import org.eclipse.che.commons.test.tck.repository.JpaTckRepository;
 import org.eclipse.che.commons.test.tck.repository.TckRepository;
 import org.eclipse.che.core.db.DBInitializer;
+import org.eclipse.che.core.db.schema.SchemaInitializer;
 
 /**
  * @author Sergii Leschenko
+ * @author Yevhenii Voevodin
  */
 public class AccountJpaTckModule extends TckModule {
     @Override
     protected void configure() {
         install(new JpaPersistModule("main"));
         bind(DBInitializer.class).asEagerSingleton();
+        bind(SchemaInitializer.class).toInstance(() -> new H2SchemaLoader().loadSilently());
+        bind(TckResourcesCleaner.class).to(H2JpaCleaner.class);
+
         bind(new TypeLiteral<TckRepository<AccountImpl>>() {}).toInstance(new JpaTckRepository<>(AccountImpl.class));
 
         bind(AccountDao.class).to(JpaAccountDao.class);
